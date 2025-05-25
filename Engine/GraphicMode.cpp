062b4431b4b0c02f2a2f481987ecce88cd572e22
KrystianGrzegorzewicz <kgrzeg@student.agh.edu.pl>
2025-05-25 20:35:08 +0200

    added GraphicMode class, this class is to switch dark/light mode and drawing behaviours

---
+++ Engine/GraphicMode.cpp
@@ -0,0 +1,1 @@
+#include "GraphicMode.h"
---
+++ Engine/GraphicMode.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class GraphicMode
+{
+public:
+	GraphicMode();
+	~GraphicMode();
+
+private:
+
+};
+
+GraphicMode::GraphicMode()
+{
+}
+
+GraphicMode::~GraphicMode()
+{
+}
