7e4bab0c0df5a9ec1912ad9cff68f857a81724e5
KrystianGrzegorzewicz <kgrzeg@student.agh.edu.pl>
2025-05-25 21:42:58 +0200

    added some definitions and dark mode switch

--- Engine/GraphicMode.cpp
+++ Engine/GraphicMode.cpp
@@ -1,1 +1,16 @@
 #include "GraphicMode.h"
+
+void GraphicMode::DrawBackground(Drawin drw) {
+
+}
+
+void GraphicMode::DrawForeground(Drawin drw) {
+
+}
+void GraphicMode::SwitchDarkMode() {
+	darkmode = !darkmode;
+	if (darkmode)
+		GraphicMode::bgColor = Colors::DarkBackGround;
+	else
+		GraphicMode::bgColor = Colors::LightBackGround;
+}
--- Engine/GraphicMode.h
+++ Engine/GraphicMode.h
@@ -1,19 +1,15 @@
 #pragma once
+#include "Drawin.h"
+//#include "Colors.h"
+
 
 class GraphicMode
 {
 public:
-	GraphicMode();
-	~GraphicMode();
-
+	static void DrawBackground(Drawin drw);
+	static void DrawForeground(Drawin drw);
+	static void SwitchDarkMode();
 private:
-
+	static bool darkmode;
+	static Color bgColor;
 };
-
-GraphicMode::GraphicMode()
-{
-}
-
-GraphicMode::~GraphicMode()
-{
-}
